4db2a8da0aa96f79828295c8998a9c51326e3485
GRsandy <143724905+GRsandy@users.noreply.github.com>
2023-09-07 09:52:18 -0500

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println(" Sandra Garduño Reyes ");
+        System.out.println(" Este programa calcula el area del rectangulo ");
+        
+        int base = 25;
+        int altura= 10;
+    
+    int AreaRectangulo = base * altura ;
+    
+     System.out.println(" El area del rectangulo " + AreaRectangulo);
+       
+    }
+    
+}
